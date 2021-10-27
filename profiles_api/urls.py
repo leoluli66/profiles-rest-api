0df82a21bd7d40d364d5ef52a0c6ae6087a2f210
Li Lu <llu85@gatech.edu>
2021-10-27 15:45:13 -0700

    Added viewset to prifiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
+
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
+    path('hello-view/', views.HelloApiView.as_view()), #APIview
+    path('', include(router.urls)) #Viewset, via Routers
 ]
